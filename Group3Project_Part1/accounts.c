65dd7945e494e1f86921cfb0dff29ab3b57e1ad8
Clayton Akers <cla00001@mix.wvu.edu>
2021-12-03 21:17:42 -0500

    Added functions to validate passwords

--- Group3Project_Part1/accounts.c
+++ Group3Project_Part1/accounts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_USERNAME 15
 #define MAX_PASSWORD 25
@@ -212,13 +213,219 @@ void removeAdmin(User* database, User root)
             printf("\nThis user does not exist.\n");
         }
     }   
-    
     else
     {
         printf("\nThis user is not the root or an administrator.\n\n");
-    }         
-    
+    }             
 }
+
+/*  PASSWORD REQUIREMENTS
+    1. Should not contain spaces
+    2. Should contain at one digit (0-9)
+    3. Length must be @ least 8 characters long
+    4. Must contain @ least one special character:
+        (Using Oracle ID Manager & Microsoft Active Dir standards, also includes extra's '*')
+        -> @    -> %    -> +    -> \    -> /
+        -> '    -> !    -> #    -> $    -> ^
+        -> ?    -> :    -> ,    -> (    -> )
+        -> {    -> }    -> [    -> ]    -> ~
+        -> -    -> _    -> .    -> *    -> `
+    5. Must contain @ least one lower case letter (a-z)
+    6. Must contain @ least one upper case letter (A-Z)
+*/
+
+//Validates a new password in separate checks with requirements above
+void validate_password(char pwd[])
+{
+    if(verify_pass_len(pwd) == 1)//Checks length  of password >= 8
+    {
+        if(verify_char_rules(pwd) == 1)//Checks that password contains at least one lower case and upper case letter
+        {
+            if(verify_int_rule(pwd) == 1)//Checks that password contains at least one integer (0-9)
+            {
+                if(verify_specialChar_rule(pwd) == 1)//Checks that password contains at least one of the valid special char's listed above
+                {
+                    printf("Valid password.\n");
+                }
+            }
+        }
+    }
+
+}
+
+//Checks that password is at least 8 characters long
+int verify_pass_len(char pwd[])
+{
+    int result;
+    int count = 0;
+    while(pwd[count] != '\0')//Counts until it reaches end of password
+    {
+        count++;
+    }
+    if(count < 7)
+    {
+        result = 0;//Sets password as invalid
+        printf("Password must be at least 8 characters long.\n");
+
+    }
+    else
+    {
+        result = 1;//Sets password at this point as valid
+    }
+
+    return result;
+}
+
+//Checks that password contains at least one lower case and upper case letter.
+int verify_char_rules(char pwd[])
+{
+    int result;
+    int lowerCharExists = 0;//Boolean check for lower case letter
+    int upperCharExists = 0;//Boolean check for upper case letter
+    int isInvalidPwd = 0;
+    int index = 0;
+    while(pwd[index] != '\0')
+    {
+        if(pwd[index + 1] == '\0')//Checks if reached end of password
+        {
+            isInvalidPwd = 1;
+        }
+        if((int)pwd[index] > 64 && (int)pwd[index] < 91)//Checks for upper case ASCII values
+        {
+            upperCharExists = 1;
+
+        }
+        if((int)pwd[index] > 96 && (int)pwd[index] < 123)//Checks for lower case ASCII values
+        {
+            lowerCharExists = 1;
+
+        }
+
+        index++;
+    }
+
+    if(isInvalidPwd == 1 && (lowerCharExists == 0 && upperCharExists == 0))
+    {
+        result = 0;
+        printf("Password requires at least one lower case and upper case letter.\n");
+    }
+    else
+    {
+        if(lowerCharExists == 0 && upperCharExists == 1)
+        {
+            result = 0;
+            printf("Password requires at least one lower case letter.\n");
+        }
+        else if(upperCharExists == 0 && lowerCharExists == 1)
+        {
+            result = 0;
+            printf("Password requires at least one upper case letter.\n");
+        }
+        else
+        {
+           result = 1;
+        }
+    }
+
+    return result;
+}
+
+//Checks that password contains at least one integer between 0-9
+int verify_int_rule(char pwd[])
+{
+    int result;
+    int containsDigit = 0;//Boolean check if there is digit in password
+    int index = 0;
+    while(pwd[index] != '\0')
+    {
+        if(isdigit(pwd[index]))//Checks if each index of password is digit
+        {
+            containsDigit = 1;
+        }
+
+        index++;
+    }
+
+    if(containsDigit == 1)
+    {
+        result = 1;
+    }
+    else
+    {
+        result = 0;
+        printf("Password must contain at least one digit between 0 and 9\n");
+    }
+
+    return result;
+}
+
+//Checks if password contains at least one valid special character
+int verify_specialChar_rule(char pwd[])
+{
+    int result;
+    int index = 0;
+    int hasSpecChar = 0;//Boolean check if password contains a special character
+    int isInvalidChar = 0;//Boolean check if password contains an invalid special character
+    while(pwd[index] != '\0')
+    {
+        //If current index of password is not a digit or a letter, checks for invalid characters
+        if(!isdigit(pwd[index]) && !isalpha(pwd[index]))
+        {
+            switch(pwd[index])
+            {
+            case ' ':
+                printf("Spaces are not allowed in password.\n");
+                isInvalidChar = 1;
+                break;
+            case ';':
+                printf("Semicolon not allowed in password.\n");
+                isInvalidChar = 1;
+                break;
+            case '|':
+                printf("Vertical slash not allowed in password.\n");
+                isInvalidChar = 1;
+                break;
+            case '"':
+                printf("Quotation not allowed in password.\n");
+                isInvalidChar = 1;
+                break;
+            case '<':
+                printf("Left angle bracket not allowed in password.\n");
+                isInvalidChar = 1;
+                break;
+            case '>':
+                printf("Right angle bracket not allowed in password.\n");
+                isInvalidChar = 1;
+                break;
+            default:
+                hasSpecChar = 1;//No invalid characters found, then valid special character was found
+                break;
+            }
+        }
+
+        index++;
+    }
+
+    if(isInvalidChar == 1)
+    {
+        result = 0;
+    }
+    else
+    {
+        if(hasSpecChar == 0)
+        {
+            printf("Password requires at least one special character.\n");
+            result = 0;
+        }
+        else
+        {
+            result = 1;
+        }
+    }
+
+    return result;
+}
+
 
 int main()
 {
